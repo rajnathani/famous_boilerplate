2d2c24800bed4a6a3d9b340e815f14aa7d394e56
relfor <relfor@outlook.com>
2014-05-28 21:47:57 -0400

    initial push

---
+++ boilerplate.sh
@@ -0,0 +1,3 @@
+cat src/unconcat/functionPrototypeBind.js src/unconcat/classList.js src/unconcat/requestAnimationFrame.js src/unconcat/require.js src/unconcat/famous.min.js > src/famous.min.js
+cat src/unconcat/functionPrototypeBind.js src/unconcat/classList.js src/unconcat/requestAnimationFrame.js src/unconcat/famous.min.js > src/famous-minus-require.min.js
+jade index.jade
